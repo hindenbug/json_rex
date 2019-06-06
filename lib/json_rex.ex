6a63587077995efde42f713be6ecf9f84094185b
hindenbug <manoj.mk27@gmail.com>
2019-06-06 22:55:38 +0200

    Setup rustler in mix project

---
+++ lib/json_rex.ex
@@ -0,0 +1,18 @@
+defmodule JsonRex do
+  @moduledoc """
+  Documentation for JsonRex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JsonRex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/json_rex_test.exs
@@ -0,0 +1,8 @@
+defmodule JsonRexTest do
+  use ExUnit.Case
+  doctest JsonRex
+
+  test "greets the world" do
+    assert JsonRex.hello() == :world
+  end
+end
